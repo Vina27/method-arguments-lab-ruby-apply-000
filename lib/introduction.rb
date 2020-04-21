de2d3f2d149aaf86f2f70c8501199a5823f8ff61
Maysoon Tabali <mtab03@me.com>
2020-04-21 15:14:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
 def introduction_with_language(name, language)
-  puts "Hi, may name is #{Josh} and I am learning to program in #{language}."
+  puts "Hi, may name is #{name} and I am learning to program in #{language}."
 end
